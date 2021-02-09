24ab26934c2b61896051349428c22cefa48e29f3
DenisYIN <yinfamin@hotmail.com>
2021-02-09 21:59:04 +0800

    first commit

---
+++ 1.py
@@ -0,0 +1,19 @@
+# Input: digits = "23"
+# Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+# create a library
+def letterCombinations(digits):
+    phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}  
+    
+
+
+
+
+print(phone)
